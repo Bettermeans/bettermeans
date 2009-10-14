f336bbcee93f2177b933ed0fa07608739eeef207
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2009-10-14 02:12:10 -0700

    Recovering from rollback

--- app/helpers/journals_helper.rb
+++ app/helpers/journals_helper.rb
@@ -17,7 +17,7 @@
 
 module JournalsHelper
   def render_notes(journal, options={})
-    votingcontent = ''
+    # votingcontent = ''
     content = ''
     editable = journal.editable_by?(User.current)
     links = []
@@ -31,38 +31,63 @@ module JournalsHelper
                                              { :controller => 'journals', :action => 'edit', :id => journal },
                                                 :title => l(:button_edit)) if editable
       
-      # Voting on journal items
-       unless journal.user_id == User.current.id ||
-    	  !User.current.logged? ||
-    	  User.current.voted_on?(journal)
-        votingcontent << link_to_remote (image_tag('/images/aupgray.gif', :size => "15x14", :border => 0), 
-        {
-    		  :url => user_journal_votes_path(User.current, journal, :vote => :true, :format => :js, :voteable_type => "journal"), 
-    		  :method => :post
-  		    })
-  		    votingcontent << link_to_remote (image_tag('/images/adowngray.gif', :size => "15x14", :border => 0), 
-          {
-    		    :url => user_journal_votes_path(User.current, journal, :vote => :false, :format => :js, :voteable_type => "journal"), 
-    		    :method => :post
-  		      })
-		    end
-		    
-		    #We show total votes regardless 
-  		  votingcontent << " " + String(journal.votes_for - journal.votes_against) + " points"
-  		  
-  		
-
     	
     		
     end
+    
+    #     # Voting on journal items
+    #     unless journal.user_id == User.current.id ||
+    #       !User.current.logged? ||
+    #       User.current.voted_on?(journal)
+    #       votingcontent << link_to_remote (image_tag('/images/aupgray.gif', :size => "15x14", :border => 0), 
+    #         {
+    #         :url => user_journal_votes_path(User.current, journal, :vote => :true, :format => :js, :voteable_type => "journal"), 
+    #         :method => :post
+    #         })
+    #       votingcontent << link_to_remote (image_tag('/images/adowngray.gif', :size => "15x14", :border => 0), 
+    #         {
+    #     :url => user_journal_votes_path(User.current, journal, :vote => :false, :format => :js, :voteable_type => "journal"), 
+    #     :method => :post
+    #     })
+    # end
+    # 
+    #     #We show total votes regardless 
+    #     votingcontent << " " + String(journal.votes_for - journal.votes_against) + " points"
+
+    
     content << content_tag('div', links.join(' '), :class => 'contextual') unless links.empty?
-    content << content_tag('div', votingcontent, :id => "votes_" + String(journal.id), :class => 'journalvote')        
+    # content << content_tag('div', votingcontent, :id => "votes_" + String(journal.id), :class => 'journalvote')        
     content << textilizable(journal, :notes)
     css_classes = "wiki"
     css_classes << " editable" if editable
     css_classes << " gravatar-margin" if Setting.gravatar_enabled?
     content_tag('div', content, :id => "journal-#{journal.id}-notes", :class => css_classes)
   end
+  
+  def render_votes(journal, options={})
+    votingcontent = ''
+    # Voting on journal items
+    unless journal.user_id == User.current.id ||
+      !User.current.logged? ||
+      User.current.voted_on?(journal)
+      votingcontent << link_to_remote (image_tag('/images/aupgray.gif', :size => "15x14", :border => 0), 
+        {
+    	  :url => user_journal_votes_path(User.current, journal, :vote => :true, :format => :js, :voteable_type => "journal"), 
+    	  :method => :post
+    	  })
+      votingcontent << link_to_remote (image_tag('/images/adowngray.gif', :size => "15x14", :border => 0), 
+        {
+  		  :url => user_journal_votes_path(User.current, journal, :vote => :false, :format => :js, :voteable_type => "journal"), 
+  		  :method => :post
+  	    })
+    end
+    
+    #We show total votes regardless 
+    votingcontent << " " + String(journal.votes_for - journal.votes_against) + " points"
+  	content_tag('span', votingcontent, :id => "votes_" + String(journal.id), :class => 'journalvote')        
+    
+  end
+  
   
   def link_to_in_place_notes_editor(text, field_id, url, options={})
     onclick = "new Ajax.Request('#{url_for(url)}', {asynchronous:true, evalScripts:true, method:'get'}); return false;"
