d3ea3465e291f0436259ec1d885c1f05a30ecfe0
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2009-10-26 16:00:25 -0700

    Fixes #249: Offer is ruined if I release ownership

--- app/models/commit_request.rb
+++ app/models/commit_request.rb
@@ -11,8 +11,12 @@ class CommitRequest < ActiveRecord::Base
   end
 
   #Returns request for current user and issue
-  def self.request(user, issue)
-    @cr = find(:first, :conditions => ["user_id = ? AND issue_id = ?", user, issue], :order => "updated_on DESC")
+  def self.request(user, issue, userowned)
+    if (userowned) #user owns this issue, we're looking for the commit request that he took this issue with (either accepting an offer, taking one)
+      @cr = find(:first, :conditions => ["responder_id = ? AND issue_id = ? AND (response = 2 or response = 6) ", user, issue], :order => "updated_on DESC")
+    else
+      @cr = find(:first, :conditions => ["user_id = ? AND issue_id = ?", user, issue], :order => "updated_on DESC")
+    end
   end
   
 end
