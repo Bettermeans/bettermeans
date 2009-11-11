fc039838a9bd13a11f8334f3cc366ec93beaa03a
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2009-11-11 10:10:58 -0800

    Offer notification, and message notification types added

--- db/migrate/20091107073735_create_notifications.rb
+++ db/migrate/20091107073735_create_notifications.rb
@@ -3,7 +3,7 @@ class CreateNotifications < ActiveRecord::Migration
     create_table :notifications do |t|
       t.integer :recipient_id
       t.string :variation #different types of notification
-      t.string :params
+      t.text :params
       t.integer :state, :default => 0 #-1 is deactivated, 0 is active, 1 is responded, 2 is archived
       t.integer :source_id #id of object that the notification is about. For example, if a commit_request is issueing the notification, this is a good place to store the commit_request_id
       t.date :expiration_date
