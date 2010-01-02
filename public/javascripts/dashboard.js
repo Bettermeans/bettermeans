f4ea13a554be32742ee44396985fec9eeb131ecd
Shereef Bishay <shereef@mbpro.local>
2010-01-02 02:09:46 -0800

    Adds new item form to dashboard. Cleans up interface and changes colors

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -3,12 +3,19 @@
 //cleaner times/dates in flyover
 //flyover should stay open if I hover over it
 //max height for flyover
-//bugbug: flyover appears even 
 //toggle buttons to turn panels on and off
-//bugbug: flyover appears when i hover over expand item button
+//help hover overs (question marks)
+//order items by priority? or updated?
+//little hover over question marks for each panel describing whay they are
+//"my work" panel
+//somewhere in the "new item" tool tip, let them know that pressing the 'n' key activates the new item (should also be a tooltip for the new request button)
+//what are the types of requests? feature? chore? or do we hide this functionality for now? keep it simple!
+
 
 
 var D; //all data
+var keyboard_shortcuts = false;
+var default_new_title = 'Enter Title Here';
 
 $(window).bind('resize', function() {
 	resize();
@@ -17,15 +24,38 @@ $(window).bind('resize', function() {
 
 $('document').ready(function(){
 	
-	//load data
-   $.get('dashdata', {project_id: projectID},
-            function(data){
+		keyboard_shortcuts = false;
+		
+	
+	   $.get('dashdata', {project_id: projectID},
+	            function(data){
 				$("#loading").hide();
 				D = data;
 				prepare_page();
-    }, 'json');
+	    }, 'json');
+	
+		load_buttons();
     
 });
+
+// listens for any navigation keypress activity
+$(document).keypress(function(e)
+{
+	if (!keyboard_shortcuts){return;};
+	
+	switch(e.which)
+	{
+		// user presses the "a"
+		case 110:	new_item();
+					break;	
+					
+	}
+});
+
+
+function load_buttons(){
+	$('#main-menu').append('<input id="new_request" value="New Request" type="submit" onclick="new_item();" class="dashboard-button" style="margin-left: 20px;"/>');
+}
 
 $.fn.makeAbsolute = function(rebase) {
 
@@ -53,36 +83,20 @@ function prepare_page(){
 	load_ui();
 	resize();
 	recalculate_widths();
+	keyboard_shortcuts = true;	
 }
 
 // Loads all items in their perspective panels, and sets up panels
 function load_ui(){
-	insert_panel(0,'new','New');
-	insert_panel(0,'open','Open');
-	insert_panel(0,'inprogress','In Progress');
-	insert_panel(0,'done','Done');
-	insert_panel(0,'canceled','Cancelled');
-	insert_panel(0,'unknown','Unsorted');
+	insert_panel(0,'new','New',true);
+	insert_panel(0,'open','Open',true);
+	insert_panel(0,'inprogress','In Progress',true);
+	insert_panel(0,'done','Done',true);
+	insert_panel(0,'canceled','Cancelled',false);
+	insert_panel(0,'unknown','Unsorted',false);
 	
 	for(var i = 0; i < D.length; i++ ){
-		var panelid = '';
-		//Deciding on wich panel for this item?
-		switch (D[i].status.name){
-		case 'Open':
-		panelid= 'open_items';
-		break;
-		case 'Committed':
-		panelid = 'inprogress_items';
-		break;
-		case 'Done':
-		panelid = 'done_items';
-		break;
-		case 'Canceled':
-		panelid = 'canceled_items';
-		break;
-		default : panelid = 'unknown_items';
-		}
-		add_item(panelid,i);	
+		add_item(i,"bottom");	
 	}
 
 	$("#fresh_items").append("<div class='endOfList></div>");
@@ -92,6 +106,10 @@ function load_ui(){
 	$("#canceled_items").append("<div class='endOfList></div>");
 	$("#unknown_items").append("<div class='endOfList></div>");
 	
+	add_hover_icon_events();	
+}
+
+function add_hover_icon_events(){
 	$(".hoverIcon").hover(
 	      function () {
 			show_flyover(Number(this.id.split('_')[1].replace(/"/g,'')));
@@ -100,8 +118,6 @@ function load_ui(){
 			hide_flyover(Number(this.id.split('_')[1].replace(/"/g,'')));
      	  }
 	    );
-	
-	
 }
 
 function show_flyover(dataId){
@@ -134,13 +150,40 @@ function hide_flyover(dataId){
 	$('#flyover_' + dataId).hide();
 }
 
-function add_item(panelid, dataId){
+function add_item(dataId,position){
+	var panelid = '';
+	//Deciding on wich panel for this item?
+	switch (D[dataId].status.name){
+	case 'New':
+	panelid= 'new_items';
+	break;
+	case 'Open':
+	panelid= 'open_items';
+	break;
+	case 'Committed':
+	panelid = 'inprogress_items';
+	break;
+	case 'Done':
+	panelid = 'done_items';
+	break;
+	case 'Canceled':
+	panelid = 'canceled_items';
+	break;
+	default : panelid = 'unknown_items';
+	}
+	
 	var html = generate_item(dataId);
-	$("#" + panelid).append(html);
+	if (position=="bottom")
+	{
+		$("#" + panelid).append(html);
+	}
+	else
+	{
+		$("#" + panelid).prepend(html);
+	}
 }
 
 function generate_flyover(dataId){
-	console.log('data id:' + dataId  + D);
 	item = D[dataId];
 	
 	var html = '';
@@ -194,7 +237,7 @@ function generate_flyover(dataId){
 
 function generate_flyover_description(item){
 
-	if (item.description.length<3){return '';};
+	if (item.description == null || item.description.length < 3){return '';};
 	
 	var html = '';
 	html = html + '	  <div class="header">';
@@ -376,9 +419,15 @@ function resize(){
 	$("#panels").show();
 }
 
-function insert_panel(position, name, title){
+function insert_panel(position, name, title, visible){
+	var panel_style = "";
+	var button_style = "";
+	if (!visible){panel_style = 'style="display:none;"';}
+	if (visible){button_style = 'style="display:none;"';}
+	// visible ? panel_style = 'block' : panel_style = 'none';
+
 	var panelHtml = '';
-	panelHtml = panelHtml + "	<td id='" + name + "_panel' class='panel'>";
+	panelHtml = panelHtml + "	<td id='" + name + "_panel' class='panel' " + panel_style + "'>";
 	panelHtml = panelHtml + "<div class='panelHeaderRight'></div>";
 	panelHtml = panelHtml + "<div class='panelHeaderLeft'></div>";
 	panelHtml = panelHtml + "<div class='panelHeader'>";
@@ -390,16 +439,20 @@ function insert_panel(position, name, title){
 	panelHtml = panelHtml + "  </div>";
 	panelHtml = panelHtml + "</div>";
 	panelHtml = panelHtml + "</td>";
+	$('#main-menu').append('<input id="' + name + '_panel_toggle" value="' + title + '" type="submit" onclick="show_panel(\'' + name + '\');" class="dashboard-button" ' + button_style + '/>');
 	$("#main_row").append(panelHtml);
 }
 
 function close_panel(name){
 	$('#' + name + '_panel').hide();
+	$('#' + name + '_panel_toggle').show();
+	
 	recalculate_widths();
 }
 
 function show_panel(name){
 	$('#' + name + '_panel').show();
+	$('#' + name + '_panel_toggle').hide();
 	recalculate_widths();
 }
 
@@ -415,6 +468,176 @@ function expand_item(dataId){
 
 function collapse_item(dataId){
 	$('#item_' + dataId).html(generate_item(dataId));
+	add_hover_icon_events();	
+}
+
+function save_new_item(){
+	// console.log( $('#[description]').val() );
+	if (($('#new_title_input').val() == default_new_title) || ($('#new_title_input').val() == ''))
+	{
+		alert('Please enter a title');
+		return false;
+	}
+	var data = "commit=Create&project_id=" + projectID + "&issue[subject]=" + $('#new_title_input').val() + "&issue[description]=" + $('#new_description').val();
+
+    var url = url_for({ controller: 'issues',
+                           action    : 'new'
+                          });
+
+	$("#new_item_wrapper").html('<div id="loading"> Adding...</div>');
+
+	$.post(url, 
+		   data, 
+		   	function(html){
+				item_added(html);
+			}, //TODO: handle errors here
+			"json" //BUGBUG: is this a security risk?
+	);
+	return false;
+}
+
+function cancel_new_item(){
+	$("#new_item_wrapper").remove();
+	keyboard_shortcuts = true;
+}
+
+function item_added(item){
+	$("#new_item_wrapper").remove();
+	D.push(item); 
+	add_item(D.length-1,"top");
+	add_hover_icon_events();
+	keyboard_shortcuts = true;
+}
+
+function new_item(){
+keyboard_shortcuts = false;
+$("#new_item_wrapper").remove();
+html = '';	
+html = html + '	<div class="item" id="new_item_wrapper">';
+html = html + '	  <div class="storyItem unscheduled unestimatedText underEdit" id="icebox_itemList_storynewStory_content">';
+html = html + '	   <form action="#">';
+html = html + '	    <div class="storyPreviewHeader">';
+html = html + '	      <div class="storyPreviewInput">';
+html = html + '	        <input id="new_title_input" class="titleInputField" name="title_input" value="" type="text">';
+html = html + '	      </div>';
+html = html + '	    </div>';
+html = html + '	    <div>';
+html = html + '	      <div id="new_details" class="storyDetails">';
+html = html + '	          <table class="storyDetailsTable">';
+html = html + '	            <tbody>';
+html = html + '	              <tr>';
+html = html + '	                <td>';
+html = html + '	                  <div class="storyDetailsButton">';
+html = html + '	                    <input id="new_save_button" value="Save" type="submit" onclick="save_new_item();">';
+html = html + '	                  </div>';
+html = html + '	                </td>';
+html = html + '	                <td>';
+html = html + '	                  <div class="storyDetailsButton">';
+html = html + '	                    <input id="new_cancel_button" value="Cancel" type="submit" onclick="cancel_new_item();">';
+html = html + '	                  </div>';
+html = html + '	                </td>';
+html = html + '	                <td>';
+html = html + '	                  <div class="storyDetailsButton">';
+html = html + '	                    <input disabled="disabled" id="new_delete_button" value="Delete" type="submit">';
+html = html + '	                  </div>';
+html = html + '	                </td>';
+html = html + '	                <td>';
+html = html + '	                  <div class="storyDetailsButton">';
+html = html + '	                    <input disabled="disabled" id="new_view_history_button" value="View history" type="submit">';
+html = html + '	                  </div>';
+html = html + '	                </td>';
+html = html + '	              </tr>';
+html = html + '	            </tbody>';
+html = html + '	          </table>';
+html = html + '	          <table class="storyDetailsTable">';
+html = html + '	            <tbody>';
+html = html + '	              <tr>';
+html = html + '	                <td class="letContentExpand" colspan="1">';
+html = html + '	                  <div>';
+html = html + '	                    <select id="new_story_type" class="storyDetailsField" name="new_story_type">';
+html = html + '	                      <option selected="true" value="feature">';
+html = html + '	                        Feature';
+html = html + '	                      </option>';
+html = html + '	                      <option value="bug">';
+html = html + '	                        Bug';
+html = html + '	                      </option>';
+html = html + '	                      <option value="chore">';
+html = html + '	                        Chore';
+html = html + '	                      </option>';
+html = html + '	                      <option value="release">';
+html = html + '	                        Release';
+html = html + '	                      </option>';
+html = html + '	                    </select>';
+html = html + '	                  </div>';
+html = html + '	                </td>';
+html = html + '	                <td class="storyDetailsLabelIcon" colspan="1">';
+html = html + '	                  <div class="storyDetailsLabelIcon">';
+html = html + '	                    <img src="/images/feature_icon.png" id="new_story_type_image" name="new_story_type_image">';
+html = html + '	                  </div>';
+html = html + '	                </td>';
+html = html + '	                <td class="helpIcon lastCell" colspan="1">';
+html = html + '	                  <div class="helpIcon" id="story_newStory_details_help_story_types">';
+html = html + '	                    <img src="/images/question_mark.gif">';
+html = html + '	                  </div>';
+html = html + '	                </td>';
+html = html + '	              </tr>';
+html = html + '	            </tbody>';
+html = html + '	          </table>';
+html = html + '	          <div class="section">';
+html = html + '	            <table class="storyDescriptionTable">';
+html = html + '	              <tbody>';
+html = html + '	                <tr>';
+html = html + '	                  <td class="header">';
+html = html + '	                    <div>';
+html = html + '	                      Description';
+html = html + '	                    </div>';
+html = html + '	                  </td>';
+html = html + '	                  <td class="lastCell">';
+html = html + '	                    <div class="helpIcon">';
+html = html + '	                      <img src="/images/question_mark.gif">';
+html = html + '	                    </div>';
+html = html + '	                  </td>';
+html = html + '	                </tr>';
+html = html + '	                <tr>';
+html = html + '	                  <td colspan="5">';
+html = html + '	                    <div>';
+html = html + '	                      <textarea class = "textAreaFocus" id="new_description" rows="3" cols="20" name="story[description]"></textarea>     ';
+html = html + '	                    <div>';
+html = html + '	                        (Format using *<b>bold</b>* and _<i>italic</i>_ text.)';
+html = html + '	                      </div>';
+html = html + '	                    </div>';
+html = html + '	                  </td>';
+html = html + '	                </tr>';
+html = html + '	              </tbody>';
+html = html + '	            </table>';
+html = html + '	          </div>';
+html = html + '	      </div>';
+html = html + '	    </div>';
+html = html + '    </form>';
+html = html + '	  </div>';
+html = html + '	</div>';
+
+show_panel('new');
+	$("#new_items").prepend(html);
+	$("#new_title_input").val(default_new_title).select();	
+}
+
+///HELPERS
+function url_for(options){
+  // THINKABOUTTHIS: Is it worth using Rails' routes for this instead?
+  var url = '/' + options['controller'] ;
+  if(options['action']!=null && options['action'].match(/index/)==null) url += '/' + options['action'];
+  if(options['id']!=null) url += "/" + options['id'];
+  
+  // var keys = Object.keys(options).select(function(key){ return (key!="controller" && key!="action" && key!="id"); });    
+  // if(keys.length>0) url += "?";
+  // 
+  // keys.each(function(key, index){
+  //   url += key + "=" + options[key];
+  //   if(index<keys.length-1) url += "&";
+  // });
+  
+  return url;
 }
 
 // 
