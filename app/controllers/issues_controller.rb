a15218efc4a6a6976a6f56f61769953340568807
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2010-01-14 11:01:25 -0800

    Adds error handling for dashboard

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -610,6 +610,7 @@ class IssuesController < ApplicationController
   
 private
   def find_issue
+    lkjlkjlkj
     @issue = Issue.find(params[:id], :include => [:project, :tracker, :status, :author, :priority])
     @project = @issue.project
     logger.info(@issue.inspect)
@@ -634,6 +635,7 @@ private
   end
   
   def find_project
+    asdfas
     @project = Project.find(params[:project_id])
   rescue ActiveRecord::RecordNotFound
     render_404
