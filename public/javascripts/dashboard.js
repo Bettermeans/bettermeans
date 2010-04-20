746914dd9b8f55ab0f69376647facb9924b01a58
Immad Naseer <immad.naseer@gmail.com>
2010-04-20 16:46:35 -0700

    The item is not editable in the canceled state as well.

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -2239,6 +2239,7 @@ $("#new_items").scrollTo( '#new_item_wrapper', 800);
 function is_item_editable(dataId) {
   return !(D[dataId].status.name == 'Committed' ||
 	   D[dataId].status.name == 'Done'      ||
+	   D[dataId].status.name == 'Canceled'  ||
 	   D[dataId].status.name == 'Archived');
 
 }
