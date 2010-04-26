257e6e0fda98e2f70d29c6dad536d05f97a75690
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2010-04-26 14:42:59 -0700

    Updating jquery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -245,6 +245,8 @@ function randomKey(size) {
 $(document).ajaxSend(function(event, request, settings) {
   if (typeof(AUTH_TOKEN) == "undefined") return;
   // settings.data is a serialized string like "foo=bar&baz=boink" (or null)
+  if (settings.type == 'GET') return; // Don't add anything to a get request let IE turn it into a POST.
+
   settings.data = settings.data || "";
   settings.data += (settings.data ? "&" : "") + "authenticity_token=" + encodeURIComponent(AUTH_TOKEN);
 });
