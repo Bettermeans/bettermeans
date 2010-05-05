eff1f423493c6aaa320160bb9c19439b3622d0f8
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2010-05-05 00:05:55 -0700

    Attempting to solve production bug with motion votes disapearing take 2

--- app/controllers/motion_votes_controller.rb
+++ app/controllers/motion_votes_controller.rb
@@ -58,6 +58,7 @@ class MotionVotesController < ApplicationController
         # flash[:notice] = @motion_vote.isbinding ? 'Your binding vote was cast' : 'Your non-binding vote was cast'
         format.js  { render :action => "cast_vote", :motion => @motion_vote.motion}        
       else
+        format.js { render :action => "error"}
         format.html { render :action => "new" }
         format.xml  { render :xml => @motion_vote.errors, :status => :unprocessable_entity }
       end
