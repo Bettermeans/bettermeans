9af508ba42ce844ef1163dd10b24dafa73cca334
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2010-05-06 18:18:39 -0700

    Fixing permissions in redmine.rb

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -132,7 +132,7 @@ Redmine::AccessControl.map do |map|
   map.project_module :motion do |map|
     map.permission :manage_motion, {:motions => [:edit, :destroy]}, :require => :admin
     map.permission :browse_motion, {:motions => [:index, :view]}, :require => :loggedin
-    map.permission :create_motion, {:motions => :create, :new}, :require => :loggedin
+    map.permission :create_motion, {:motions => [:create, :new]}, :require => :loggedin
     map.permission :vote_motion, {:motions => :reply, :motion_vote => :create}, :require => :loggedin
   end
   
