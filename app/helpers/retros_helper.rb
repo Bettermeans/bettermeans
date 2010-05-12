6bb25aee5445827c87048ba7df30ea1d4b569d2c
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2010-05-12 12:53:51 -0700

    Fixes cron job that updates issue status

--- app/helpers/retros_helper.rb
+++ app/helpers/retros_helper.rb
@@ -11,4 +11,20 @@ module RetrosHelper
   def team_from_issue(issue)
     issue.team.collect{|iv| link_to_user_from_id iv.user_id }.join(", ")
   end
+  
+  def tame_bias(number)
+    if number.nil? 
+      return ""
+    else
+      number > 0 ? "Self Bias: +#{number.round}%" : number == "None" ? number.round : "Self Bias: -#{number.round}%"
+    end
+  end
+
+  def tame_magnitude(number)
+    if number.nil?
+      ""
+    else
+      number > 0 ? "Off by: #{number.round} points" : number == "No Bias" ? number.round : "Off by #{number.round} points"
+    end
+  end
 end
--- app/models/retro_rating.rb
+++ app/models/retro_rating.rb
@@ -3,6 +3,13 @@
 #
 
 class RetroRating < ActiveRecord::Base
+  
+  TEAM_AVERAGE = -1 #retro ratings with a rater id of -1 represent the team average for the ratee
+  FINAL_AVERAGE = -2 #final distribution for the ratee
+  SELF_BIAS = -3 #difference between self assesment and final average as a percentage of final
+  SCALE_BIAS = -4 #sum of differences in percentage points between user's assessment of self and others, and final average
+  
+  
   belongs_to :retro
   belongs_to :rater, :class_name => 'User', :foreign_key => 'rater_id'
   belongs_to :ratee, :class_name => 'User', :foreign_key => 'ratee_id'
