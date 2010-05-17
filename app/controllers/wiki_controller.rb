7cc38ac01083a1357d0eac5eff509af7661396b7
Shereef Bishay <shereef@mbpro.local>
2010-05-17 00:39:49 -0700

    Enables tinemce for wiki editor

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -15,16 +15,16 @@ class WikiController < ApplicationController
   include AttachmentsHelper   
   helper :watchers
 
-  # uses_tiny_mce :options => {:theme => 'advanced',
-  #   :browsers => %w{msie gecko},
-  #   :theme_advanced_toolbar_location => "top",
-  #   :theme_advanced_toolbar_align => "left",
-  #   :theme_advanced_resizing => true,
-  #   :theme_advanced_resize_horizontal => false,
-  #   :theme_advanced_buttons1 => %w{formatselect fontselect fontsizeselect bold italic underline strikethrough separator justifyleft justifycenter justifyright indent outdent separator bullist numlist forecolor backcolor separator link unlink image undo redo},
-  #   :theme_advanced_buttons2 => [],
-  #   :theme_advanced_buttons3 => []
-  # }, :only => [:edit]
+  uses_tiny_mce :options => {:theme => 'advanced',
+    :browsers => %w{msie gecko},
+    :theme_advanced_toolbar_location => "top",
+    :theme_advanced_toolbar_align => "left",
+    :theme_advanced_resizing => true,
+    :theme_advanced_resize_horizontal => false,
+    :theme_advanced_buttons1 => %w{formatselect fontselect fontsizeselect bold italic underline strikethrough separator justifyleft justifycenter justifyright indent outdent separator bullist numlist forecolor backcolor separator link unlink image undo redo},
+    :theme_advanced_buttons2 => [],
+    :theme_advanced_buttons3 => []
+  }, :only => [:edit]
   
   # display a page (in editing mode if it doesn't exist)
   def index
