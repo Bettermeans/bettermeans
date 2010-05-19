8e4cd6d9064afe0e480346c952dae9c588470f92
Shereef Bishay <shereef@mbpro.myhome.westell.com>
2010-05-18 20:51:09 -0700

    Removes vote_fu plugin, voting functionality and karma and moves rails outside vendor directory

--- config/environment.rb
+++ config/environment.rb
@@ -44,7 +44,7 @@ Rails::Initializer.run do |config|
 
   # Make Active Record use UTC-base instead of local time
   # config.active_record.default_timezone = :utc
-  
+
   # Use Active Record's schema dumper instead of SQL when creating the test database
   # (enables use of different database adapters for development and test environments)
   # config.active_record.schema_format = :ruby
--- vendor/plugins/vote_fu/generators/vote_fu/vote_fu_generator.rb
+++
@@ -1,8 +0,0 @@
-class VoteFuGenerator < Rails::Generator::Base 
-
-  def manifest 
-    record do |m| 
-      m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => 'vote_fu_migration'
-    end 
-  end
-end
