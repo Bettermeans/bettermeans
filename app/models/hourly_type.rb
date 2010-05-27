da538cae22e3701deda160764fde2c71c89b55a0
Immad Naseer <immad.naseer@gmail.com>
2010-05-27 13:52:39 -0700

    Small changes to the hourly types migration. Added a few validation checks to the hourly type model.

--- app/models/hourly_type.rb
+++ app/models/hourly_type.rb
@@ -1,5 +1,10 @@
 class HourlyType < ActiveRecord::Base
   belongs_to :project
+  
+  validates_presence_of :name, :project
+  
+  validates_length_of :name,        :maximum => 255
+  validates_length_of :description, :maximum => 1000, :allow_nil => true
 end
 
 # == Schema Information
