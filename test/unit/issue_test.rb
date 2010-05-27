aab5050d03672a748e8365230e512273e1155f84
Immad Naseer <immad.naseer@gmail.com>
2010-05-26 18:41:39 -0700

    Schema changes reflected in the comments produced by running rake:migrate.

--- test/unit/issue_test.rb
+++ test/unit/issue_test.rb
@@ -588,6 +588,7 @@ end
 
 
 
+
 # == Schema Information
 #
 # Table name: issues
@@ -627,5 +628,8 @@ end
 #  agree_total_nonbind  :integer         default(0)
 #  points_nonbind       :integer         default(0)
 #  pri_nonbind          :integer         default(0)
+#  hourly_type_id       :integer
+#  num_hours            :integer         default(0)
+#  is_hourly            :boolean         default(FALSE)
 #
 
