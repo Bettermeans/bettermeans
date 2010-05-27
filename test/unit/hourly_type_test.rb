f2ab0db325ab7beeb914d1bac3827f72ab9379b5
Immad Naseer <immad.naseer@gmail.com>
2010-05-27 13:43:51 -0700

    Added the hourly type model and specified the relations between the hourly type and project models.

---
+++ test/unit/hourly_type_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class HourlyTypeTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
+
+# == Schema Information
+#
+# Table name: hourly_types
+#
+#  id          :integer         not null, primary key
+#  name        :string(255)     not null
+#  description :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
