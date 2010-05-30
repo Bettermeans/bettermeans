bd79594838ed1bd22bdc6841e32c247149b6df2c
Immad Naseer <immad.naseer@gmail.com>
2010-05-30 16:20:00 -0700

    Added hourly rate per person and hourly cap fields to the hourly type. Modified the code to display this information in the dashboard.

--- app/models/hourly_type.rb
+++ app/models/hourly_type.rb
@@ -2,22 +2,26 @@ class HourlyType < ActiveRecord::Base
   belongs_to :project
   has_many :issues
   
-  validates_presence_of :name, :project
+  validates_presence_of :name, :project, :hourly_rate_per_person, :hourly_cap
+  validates_length_of :name,        :maximum => 255  
   
-  validates_length_of :name,        :maximum => 255
-  validates_length_of :description, :maximum => 1000, :allow_nil => true
+  def name_with_rates
+    "#{name} -- rate per person: #{hourly_rate_per_person}, cap: #{hourly_cap}"
+  end
 end
+
 
 
 # == Schema Information
 #
 # Table name: hourly_types
 #
-#  id          :integer         not null, primary key
-#  project_id  :integer
-#  name        :string(255)
-#  description :text
-#  created_at  :datetime
-#  updated_at  :datetime
+#  id                     :integer         not null, primary key
+#  project_id             :integer
+#  name                   :string(255)
+#  hourly_rate_per_person :decimal(8, 2)
+#  hourly_cap             :decimal(8, 2)
+#  created_at             :datetime
+#  updated_at             :datetime
 #
 
