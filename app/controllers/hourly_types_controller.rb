e0105c1610e189042bc4825a795bda5593da51a3
Immad Naseer <immad.naseer@gmail.com>
2010-05-30 00:34:46 -0700

    Added support for adding/editing/deleting hourly types.
    Haven't dealt w/ the security aspect of this -- right now, it is
    piggybacking on the security of the 'discussion boards' at places and
    doing no security checks at others.

---
+++ app/controllers/hourly_types_controller.rb
@@ -0,0 +1,33 @@
+class HourlyTypesController < ApplicationController
+  before_filter :find_project
+  
+  def new
+    @hourly_type = HourlyType.new(params[:hourly_type])
+    @hourly_type.project = @project
+    if request.post? && @hourly_type.save
+      flash[:notice] = l(:notice_successful_create)
+      redirect_to :controller => 'projects', :action => 'settings', :id => @project, :tab => 'hourly_types'
+    end
+  end  
+  
+  def edit
+    if request.post? && @hourly_type.update_attributes(params[:hourly_type])
+      redirect_to :controller => 'projects', :action => 'settings', :id => @project, :tab => 'hourly_types'
+    end
+  end
+  
+  def destroy
+    @hourly_type.destroy
+    redirect_to :controller => 'projects', :action => 'settings', :id => @project, :tab => 'hourly_types'
+  end
+  
+  
+  private  
+
+  def find_project
+    @project = Project.find(params[:project_id])
+    @hourly_type = @project.hourly_types.find(params[:id]) if params[:id]
+  rescue ActiveRecord::RecordNotFound
+    render_404
+  end
+end
