f644e105f7408b33a297d7778057f0b23d3ea007
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2010-06-03 16:46:54 -0700

    Changes conditions for running lazy majority, and condition for moving an item to open to depend on issue creation date as opposed to last time the issue was updated

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -58,7 +58,7 @@ class Issue < ActiveRecord::Base
   def ready_for_open?
     return false if points.nil? || agree_total < 1
     return true if agree + disagree > points_from_credits / 2
-    return true if agree_total > 0 && self.updated_on < DateTime.now - Setting::LAZY_MAJORITY_LENGTH
+    return true if agree_total > 0 && self.created_on < DateTime.now - Setting::LAZY_MAJORITY_LENGTH
     return true if agree_total > (project.active_binding_members_count / 2)
     return true if agree_total > 0 && self.status == IssueStatus.open
     return false
