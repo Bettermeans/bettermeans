74520e6066d9e1bbb5bb52b3e4b7eb2eaabeca30
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2010-06-04 16:47:22 -0700

    Clears highlights for fast search when clear is hit. Completes #1127

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -529,15 +529,7 @@ class Issue < ActiveRecord::Base
                                                       :old_value => @issue_before_change.send(c),
                                                       :value => send(c)) unless send(c)==@issue_before_change.send(c)
       }
-      # custom fields changes
-      custom_values.each {|c|
-        next if (@custom_values_before_change[c.custom_field_id]==c.value ||
-                  (@custom_values_before_change[c.custom_field_id].blank? && c.value.blank?))
-        @current_journal.details << JournalDetail.new(:property => 'cf', 
-                                                      :prop_key => c.custom_field_id,
-                                                      :old_value => @custom_values_before_change[c.custom_field_id],
-                                                      :value => c.value)
-      }      
+
       @current_journal.save
     end
   end
