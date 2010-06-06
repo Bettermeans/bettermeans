f289a7aaf123c1a0240ae2eb4c699bc1d5b3718e
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2010-06-06 01:26:54 -0700

    Fixing mail issues for forgot password.

--- app/models/journal_observer.rb
+++ app/models/journal_observer.rb
@@ -4,6 +4,7 @@
 
 class JournalObserver < ActiveRecord::Observer
   def after_create(journal)
+    Mailer.send(:deliver_issue_edit,journal) if Setting.notified_events.include?('issue_updated')
     Mailer.send_later(:deliver_issue_edit,journal) if Setting.notified_events.include?('issue_updated')
   end
 end
