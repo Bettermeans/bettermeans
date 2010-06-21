20c40813dc549b9a42fd2a9e3bed8c05bc7a7075
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2010-06-21 14:57:22 -0700

    adds ability to start ideas from lightbox. Completes #972

--- public/javascripts/issue.js
+++ public/javascripts/issue.js
@@ -1,8 +1,12 @@
-function show_item(){
+function show_item(startable){
 	timer_active = false;
 	D = [];
 	D.push(item);
 	$("#item_header").html(generate_item_lightbox(0));
-	
 	$("#todo_wrapper").replaceWith(generate_todo_section_lightbox(0));
+	if (startable){
+		console.log("starting...");
+		$("#item_content_buttons_start_button_0").show();
+	}
+	
 }
