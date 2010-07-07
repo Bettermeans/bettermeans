0ddf7dc5f41d6e1b375cf8825973733167e3f4c8
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2010-07-07 12:14:46 -0700

    Grouping for activity streams improved

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,7 @@ class CommentsController < ApplicationController
   log_activity_streams :current_user, :name, :updated, :@issue, :subject, :create, :issues, {:indirect_object => :@journal,
             :indirect_object_name_method => :notes,
             :indirect_object_phrase => ' ' }
+
   
     
   def index
@@ -14,10 +15,6 @@ class CommentsController < ApplicationController
                                           :order => "#{Journal.table_name}.created_on DESC",
                                           :conditions => "notes!=''")
     # render :partial => "comment", :collection => @journals, :as => :journal
-  end
-  
-  def j
-    @issue.journals.first
   end
   
   def create
