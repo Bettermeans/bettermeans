1d0591d2617e20b8d8c0a074fe298b83438d08a5
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2010-07-08 22:56:24 -0700

    Hacking away at threaded activity view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -296,27 +296,24 @@ class ProjectsController < ApplicationController
     @oustanding_credits = @credits.find_all{|credit| credit.settled_on.nil? == true }.group_by{|credit| credit.owner_id}
     @total_credits = @credits.group_by{|credit| credit.owner_id}
   end
-  
-  
+
   def activity
-    @days = Setting.activity_days_default.to_i
+    @length = Setting::ACTIVITY_STREAM_LENGTH
     
-    if params[:from]
-      begin; @date_to = params[:from].to_date + 1; rescue; end
+    if params[:length]
+      begin; @lenght = params[:length].to_i; rescue; end
     end
 
-    @date_to ||= Date.today + 1
-    @date_from = @date_to - @days
     @with_subprojects = params[:with_subprojects].nil? ? Setting.display_subprojects_issues? : (params[:with_subprojects] == '1')
+
     @author = (params[:user_id].blank? ? nil : User.active.find(params[:user_id]))
     
-    @activity = Redmine::Activity::Fetcher.new(User.current, :project => @project, 
-                                                             :with_subprojects => @with_subprojects,
-                                                             :author => @author)
-    @activity.scope_select {|t| !params["show_#{t}"].nil?}
-    @activity.scope = (@author.nil? ? :default : :all) if @activity.scope.empty?
-
-    events = @activity.events(@date_from, @date_to)
+    @activity = ActivityStream.all.group_by {|a| a.object_type + a.object_id.to_s}
+    @activity.each_pair do |key,value| 
+      @activity[key] = value.sort_by{|i| - i[:updated_at].to_i}
+    end
+    
+    @activity = @activity.sort_by{|g| - g[1][0][:updated_at].to_i}
     
     if events.empty? || stale?(:etag => [events.first, User.current])
       respond_to do |format|
@@ -338,7 +335,49 @@ class ProjectsController < ApplicationController
     
   rescue ActiveRecord::RecordNotFound
     render_404
-  end
+  end  
+  
+  # def activity
+  #   @days = Setting.activity_days_default.to_i
+  #   
+  #   if params[:from]
+  #     begin; @date_to = params[:from].to_date + 1; rescue; end
+  #   end
+  # 
+  #   @date_to ||= Date.today + 1
+  #   @date_from = @date_to - @days
+  #   @with_subprojects = params[:with_subprojects].nil? ? Setting.display_subprojects_issues? : (params[:with_subprojects] == '1')
+  #   @author = (params[:user_id].blank? ? nil : User.active.find(params[:user_id]))
+  #   
+  #   @activity = Redmine::Activity::Fetcher.new(User.current, :project => @project, 
+  #                                                            :with_subprojects => @with_subprojects,
+  #                                                            :author => @author)
+  #   @activity.scope_select {|t| !params["show_#{t}"].nil?}
+  #   @activity.scope = (@author.nil? ? :default : :all) if @activity.scope.empty?
+  # 
+  #   events = @activity.events(@date_from, @date_to)
+  #   
+  #   if events.empty? || stale?(:etag => [events.first, User.current])
+  #     respond_to do |format|
+  #       format.html { 
+  #         @events_by_day = events.group_by(&:event_date)
+  #         render :layout => false if request.xhr?
+  #       }
+  #       format.atom {
+  #         title = l(:label_activity)
+  #         if @author
+  #           title = @author.name
+  #         elsif @activity.scope.size == 1
+  #           title = l("label_#{@activity.scope.first.singularize}_plural")
+  #         end
+  #         render_feed(events, :title => "#{@project || Setting.app_title}: #{title}")
+  #       }
+  #     end
+  #   end
+  #   
+  # rescue ActiveRecord::RecordNotFound
+  #   render_404
+  # end
   
 private
   # Find project of id params[:id]
