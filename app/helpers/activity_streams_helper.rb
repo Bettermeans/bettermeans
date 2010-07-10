8ef42f1470586d5d282eaa95125315d1dcab84a1
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2010-07-09 23:40:26 -0700

    Moving partial helpers

--- app/helpers/activity_streams_helper.rb
+++ app/helpers/activity_streams_helper.rb
@@ -1,19 +1,4 @@
 module ActivityStreamsHelper
   
-  def render_journal_details(journal)
-    html = "<ul>"
-    for detail in journal.details
-      html << "<li>#{show_detail(detail)}</li>"
-    end
-    html << "</ul>"
-    
-    content = ""
-    content << textilizable(journal, :notes)
-    css_classes = "wiki"
-    css_classes << " gravatar-margin" if Setting.gravatar_enabled?
-    
-    html << content #_tag('div', content, :id => "journal-#{journal.id}-notes", :class => css_classes)
-    
-  end
   
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -771,6 +771,23 @@ module ApplicationHelper
     # end
   end
   
+  def render_journal_details(journal)
+    html = "<ul>"
+    for detail in journal.details
+      html << "<li>#{show_detail(detail)}</li>"
+    end
+    html << "</ul>"
+    
+    content = ""
+    content << textilizable(journal, :notes)
+    css_classes = "wiki"
+    css_classes << " gravatar-margin" if Setting.gravatar_enabled?
+    
+    html << content #_tag('div', content, :id => "journal-#{journal.id}-notes", :class => css_classes)
+    
+  end
+  
+  
   def url_for_activity_stream(as)  
     case as.object_type.downcase
     when 'message'
