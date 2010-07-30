9a79ab255cc14aea21a6b0502e4112ae48558985
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2010-07-30 00:12:50 -0700

    Redesign of home page and browse page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,10 +6,10 @@ class WelcomeController < ApplicationController
   caches_action :robots
 
   def index
-    @news = News.latest User.current
+    # @news = News.latest User.current
     # @projects = Project.latest User.current, 10, false
-    @enterprises = Project.latest User.current, 10, true
-    @activities_by_item = ActivityStream.fetch(nil, nil, true, 50)    
+    # @enterprises = Project.latest User.current, 10, true
+    # @activities_by_item = ActivityStream.fetch(nil, nil, true, 50)    
     @my_projects = User.current.owned_projects
     @belong_to_projects = User.current.belongs_to_projects
   end
