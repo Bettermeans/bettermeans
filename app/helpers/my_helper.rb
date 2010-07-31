b1f7a2e8a167eaf70094a17ac2ee83a73c4e92aa
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2010-07-30 18:26:11 -0700

    Basic subscription functionality

--- app/helpers/my_helper.rb
+++ app/helpers/my_helper.rb
@@ -6,4 +6,12 @@ module MyHelper
   def describe(amount)
     amount == -1 ? 'unlimited' : amount
   end
+  
+  def upgrade_options(user)
+    if user.plan.code == Plan::FREE_CODE
+      link_to "Upgrade", {:controller => :my, :action => :upgrade}, :class => "gt-btn-blue-large"
+    else
+      link_to "Upgrade / Downgrade", {:controller => :my, :action => :upgrade}, :class => "gt-btn-blue-large"
+    end
+  end
 end
