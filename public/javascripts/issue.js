dc247d978d71700f5e7e9b5e8b587cebd57d014f
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2010-08-01 21:04:03 -0700

    Issue view UI overhaul. Finishes #870

--- public/javascripts/issue.js
+++ public/javascripts/issue.js
@@ -5,7 +5,6 @@ function show_item(startable){
 	$("#item_header").html(generate_item_lightbox(0));
 	$("#todo_wrapper").replaceWith(generate_todo_section_lightbox(0));
 	if (startable){
-		console.log("starting...");
 		$("#item_content_buttons_start_button_0").show();
 	}
 	
