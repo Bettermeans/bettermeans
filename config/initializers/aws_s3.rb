729430b29468d30ef748110c2d336836a08b4c0d
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2010-08-04 11:55:35 -0700

    Changing s3 creds

--- config/initializers/aws_s3.rb
+++ config/initializers/aws_s3.rb
@@ -2,6 +2,6 @@ AWS::S3::Base.establish_connection!(
   # :access_key_id     => ENV['s3_access_key_id'],
   #    :secret_access_key => ENV['s3_secret_access_key']
   #   
-  :access_key_id     => 'AKIAIIWWFZUEBUMUF74Q',
-  :secret_access_key => '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'
+  :access_key_id     => 'AKIAI7GAIRZ5YAWMYD2A',
+  :secret_access_key => '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'
 )
