1960a4b03ba6e31989139eba38da6513c45af57a
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2010-08-03 17:12:37 -0700

    Logo for workstreams

--- config/initializers/aws_s3.rb
+++ config/initializers/aws_s3.rb
@@ -1,4 +1,7 @@
 AWS::S3::Base.establish_connection!(
-  :access_key_id     => ENV['s3_access_key_id'],
-  :secret_access_key => ENV['s3_secret_access_key']
+  # :access_key_id     => ENV['s3_access_key_id'],
+  # :secret_access_key => ENV['s3_secret_access_key']
+  
+  :access_key_id     => 'AKIAIIWWFZUEBUMUF74Q',
+  :secret_access_key => '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'
 )
