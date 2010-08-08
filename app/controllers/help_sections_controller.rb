fe9742e195c96df2999027742f02c194cbfd8934
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2010-08-07 21:06:55 -0700

    First use help sections. Finishes #1129

---
+++ app/controllers/help_sections_controller.rb
@@ -0,0 +1,100 @@
+class HelpSectionsController < ApplicationController
+  # GET /help_sections
+  # GET /help_sections.xml
+  def index
+    @help_sections = HelpSection.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @help_sections }
+    end
+  end
+
+  # GET /help_sections/1
+  # GET /help_sections/1.xml
+  def show
+
+    respond_to do |format|
+      if @help_section.show
+        format.html # show.html.erb
+        format.xml  { render :xml => @help_section }
+      else
+        format.html { render :nothing => true}
+      end
+    end
+  end
+  
+  def dont_show
+    @help_section = HelpSection.find(params[:id])
+    @help_section.show = false
+    @help_section.save
+    respond_to do |wants|
+      wants.js { render :update do |page|
+                    page.replace "help_section", ""
+                  end
+          }
+    end
+  end
+
+  # GET /help_sections/new
+  # GET /help_sections/new.xml
+  def new
+    @help_section = HelpSection.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @help_section }
+    end
+  end
+
+  # GET /help_sections/1/edit
+  def edit
+    @help_section = HelpSection.find(params[:id])
+  end
+
+  # POST /help_sections
+  # POST /help_sections.xml
+  def create
+    @help_section = HelpSection.new(params[:help_section])
+
+    respond_to do |format|
+      if @help_section.save
+        flash[:notice] = 'HelpSection was successfully created.'
+        format.html { redirect_to(@help_section) }
+        format.xml  { render :xml => @help_section, :status => :created, :location => @help_section }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @help_section.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /help_sections/1
+  # PUT /help_sections/1.xml
+  def update
+    @help_section = HelpSection.find(params[:id])
+
+    respond_to do |format|
+      if @help_section.update_attributes(params[:help_section])
+        flash[:notice] = 'HelpSection was successfully updated.'
+        format.html { redirect_to(@help_section) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @help_section.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /help_sections/1
+  # DELETE /help_sections/1.xml
+  def destroy
+    @help_section = HelpSection.find(params[:id])
+    @help_section.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(help_sections_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
