458f55847d91b62507051575aabfeb7b1923454c
Shereef Bishay <shereef@mbpro.gateway.2wire.net>
2010-08-31 13:59:18 -0700

    Fixes activity streams

--- app/models/activity_stream.rb
+++ app/models/activity_stream.rb
@@ -91,9 +91,9 @@ class ActivityStream < ActiveRecord::Base
 
     conditions = {}
     conditions[:actor_id] = user_id unless user_id.nil? || with_subprojects == "custom"
-    # conditions[:project_id] = user.projects.collect{|m| m.id} if !user.nil? && with_subprojects == "custom" && !user.projects.empty? #Customized activity stream for user
-    # conditions[:project_id] = project.id if project && !with_subprojects
-    # conditions[:project_id] = project.sub_project_array_visible_to(User.current) if project && with_subprojects
+    conditions[:project_id] = user.projects.collect{|m| m.id} if !user.nil? && with_subprojects == "custom" && !user.projects.empty? #Customized activity stream for user
+    conditions[:project_id] = project.id if project && !with_subprojects
+    conditions[:project_id] = project.sub_project_array_visible_to(User.current) if project && with_subprojects
     
     project_specified = conditions[:project_id] #temp variable for later use
     
