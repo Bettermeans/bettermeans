7132e7ff5c02355f19914f12ea2179a8aed46935
Shereef Bishay <shereef@gmail.com>
2010-11-29 17:03:15 -0800

    Changing welcome to adele

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -292,7 +292,7 @@ class Mailer < ActionMailer::Base
   #   issue_add(issue) => tmail object
   #   Mailer.deliver_issue_add(issue) => sends an email to issue recipients
   def personal_welcome(user,project)
-    from "shereef@bettermeans.com"
+    from "adele@bettermeans.com"
     recipients user.mail
     subject "bettermeans and " + project.name
     body :name => user.firstname,
