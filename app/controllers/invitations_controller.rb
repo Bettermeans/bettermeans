fa5fab32a80fefd3f758c87d826109e8000c07a4
Shereef Bishay <shereef@gmail.com>
2010-12-08 15:34:08 -0800

    Trying to fix newline in email invitations take 2

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -65,8 +65,7 @@ class InvitationsController < ApplicationController
       @invitation.project_id = @project.id
       @invitation.user_id = User.current.id
       if @invitation.save
-        logger.info { "note: #{params[:note].inspect} cleaned #{cleanup_newline(params[:note]).inspect}" }
-        @invitation.deliver(cleanup_newline(params[:note]))
+        @invitation.deliver(simple_format_without_paragraph(params[:note]))
         success = true
       end
     end
