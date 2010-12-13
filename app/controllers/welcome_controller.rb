65d97ee6e099916e51c26a4bc08407dd1c5003c7
Shereef Bishay <shereef@gmail.com>
2010-12-13 13:35:06 -0800

    Moved projects to seperate workstreams page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -15,6 +15,18 @@ class WelcomeController < ApplicationController
     # @activities_by_item = ActivityStream.fetch(nil, nil, true, 50)    
     @my_projects = User.current.owned_projects
     @belong_to_projects = User.current.belongs_to_projects
+    
+    @news = News.find(:all,
+                             :limit => 10,
+                             :order => "#{News.table_name}.created_at DESC",
+                             :conditions => "#{News.table_name}.project_id in (#{User.current.projects.collect{|m| m.id}.join(',')}) AND (created_at > '#{Time.now.advance :days => (Setting::DAYS_FOR_LATEST_NEWS * -1)}')",
+                             :include => [:project, :author]) unless User.current.projects.empty?
+    
+    @assigned_issues = Issue.visible.open.find(:all, 
+                                    :conditions => {:assigned_to_id => User.current.id},
+                                    # :limit => 10, 
+                                    :include => [ :status, :project, :tracker ], 
+                                    :order => "#{Issue.table_name}.updated_at DESC")
   end
   
   def robots
