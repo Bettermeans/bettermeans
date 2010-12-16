20de4427870669c9131e3ad031106ecf2bb44cf6
Shereef Bishay <shereef@gmail.com>
2010-12-16 13:52:04 -0800

    Fixing error with parsing poor html

--- lib/string.rb
+++ lib/string.rb
@@ -2,29 +2,33 @@ require 'rexml/parsers/pullparser'
 
 class String
   def truncate_html(len = 30)
-    p = REXML::Parsers::PullParser.new(self)
-    tags = []
-    new_len = len
-    results = ''
-    while p.has_next? && new_len > 0
-      p_e = p.pull
-      case p_e.event_type
-      when :start_element
-        tags.push p_e[0]
-        results << "<#{tags.last} #{attrs_to_s(p_e[1])}>"
-      when :end_element
-        results << "</#{tags.pop}>"
-      when :text
-        results << p_e[0].first(new_len)
-        new_len -= p_e[0].length
-      else
-        results << "<!-- #{p_e.inspect} -->"
+    begin
+      p = REXML::Parsers::PullParser.new(self)
+      tags = []
+      new_len = len
+      results = ''
+      while p.has_next? && new_len > 0
+        p_e = p.pull
+        case p_e.event_type
+        when :start_element
+          tags.push p_e[0]
+          results << "<#{tags.last} #{attrs_to_s(p_e[1])}>"
+        when :end_element
+          results << "</#{tags.pop}>"
+        when :text
+          results << p_e[0].first(new_len)
+          new_len -= p_e[0].length
+        else
+          results << "<!-- #{p_e.inspect} -->"
+        end
       end
+      tags.reverse.each do |tag|
+        results << "</#{tag}>"
+      end
+      results
+    rescue
+      self
     end
-    tags.reverse.each do |tag|
-      results << "</#{tag}>"
-    end
-    results
   end
 
   private
