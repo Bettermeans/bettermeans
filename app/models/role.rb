440151c130d5346d5f1febd53ed0de21ec28ca3d
Shereef Bishay <shereef@gmail.com>
2010-12-29 01:05:18 -0800

    rake task to re-add folks that have accepted their invitations

--- app/models/role.rb
+++ app/models/role.rb
@@ -105,7 +105,8 @@ class Role < ActiveRecord::Base
 
   # Return true if the role belongs to the enterprise (i.e. contributor, member, coreateam, admin, or board)
   def enterprise_member?
-    builtin == BUILTIN_CONTRIBUTOR || builtin == BUILTIN_CORE_MEMBER || builtin == BUILTIN_MEMBER || builtin == BUILTIN_ADMINISTRATOR || builtin == BUILTIN_BOARD
+    level == LEVEL_ENTERPRISE
+    # builtin == BUILTIN_CONTRIBUTOR || builtin == BUILTIN_CORE_MEMBER || builtin == BUILTIN_MEMBER || builtin == BUILTIN_ADMINISTRATOR || builtin == BUILTIN_BOARD
   end
 
   # Return true if the role is a binding member role
