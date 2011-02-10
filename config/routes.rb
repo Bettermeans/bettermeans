6dc07da60e5d1aaaf1b40561b2b15568dff1fb2f
Shereef Bishay <shereef@gmail.com>
2011-02-09 20:47:14 -0800

    Makes dashboard the default view for workstreams

--- config/routes.rb
+++ config/routes.rb
@@ -232,8 +232,10 @@ ActionController::Routing::Routes.draw do |map|
       project_views.connect 'projects/index_latest', :action => 'index_latest'
       project_views.connect 'projects/index_active', :action => 'index_active'
       project_views.connect 'projects/update_scale', :action => 'update_scale'
-      project_views.connect 'projects/:id', :action => 'show'
-      project_views.connect 'projects/:id/:action', :action => /roadmap|changelog|destroy|settings|team|wiki|join_core_team|leave_core_team|core_vote|dashdata|new_dashdata|dashboard|mypris|agree|disagree|accept|reject|credits|shares|community_members|community_members_array|hourly_types|map|join|reset_invitation_code/
+      project_views.connect 'projects/:id', :action => 'dashboard'
+      project_views.connect 'projects/:id/show', :action => 'dashboard'
+      project_views.connect 'projects/:id/overview', :action => 'overview'
+      project_views.connect 'projects/:id/:action', :action => /roadmap|changelog|destroy|settings|team|wiki|join_core_team|leave_core_team|core_vote|dashdata|new_dashdata|dashboard|mypris|agree|disagree|accept|reject|credits|shares|community_members|community_members_array|hourly_types|map|join|overview|reset_invitation_code|overview/
       project_views.connect 'projects/:id/files', :action => 'list_files'
       project_views.connect 'projects/:id/files/new', :action => 'add_file'
       project_views.connect 'projects/:id/settings/:tab', :action => 'settings'
