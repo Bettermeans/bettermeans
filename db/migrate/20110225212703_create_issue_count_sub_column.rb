4b6534a9eec7fc54fb4d984d6be649f8f13fafd1
Shereef Bishay <shereef@gmail.com>
2011-02-25 13:51:03 -0800

    Adds last_item_sub_updated_on

--- db/migrate/20110225212703_create_issue_count_sub_column.rb
+++ db/migrate/20110225212703_create_issue_count_sub_column.rb
@@ -1,6 +1,7 @@
 class CreateIssueCountSubColumn < ActiveRecord::Migration
   def self.up
     add_column :projects, :issue_count_sub, :integer, :default => 0, :null => false
+    Project.all.each {|p| p.refresh_issue_count}
   end
 
   def self.down
