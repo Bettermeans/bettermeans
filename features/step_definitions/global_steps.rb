4ba3a0d15dd3533656f2780fbc46cc72a29080af
Ben Biddington <ben.biddington@gmail.com>
2011-03-16 16:51:56 +1300

    Step adds new user to database before signing in as that new user

--- features/step_definitions/global_steps.rb
+++ features/step_definitions/global_steps.rb
@@ -1,5 +1,22 @@
+def create_account(username, password)
+  fail "Missing username" if username.empty?
+  
+  new_user = User.new
+  new_user.login = username    
+  new_user.firstname = username
+  new_user.lastname = username
+  new_user.password = password 
+  new_user.mail = "xxx@xxx.com"
+
+  new_user.save(:validate => true)
+  new_user
+end
+
 Given /^I am logged in as ([^\"]*)$/ do |username|
+  @logged_in_as = create_account username,username
+  
   Then "Login in as #{username} with password #{username}"
+  
   assert_not_contain "Invalid user or password"
 end
 
