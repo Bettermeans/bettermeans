6f6cb2cdfe651238abe932ee7f27a1d1f7aa9f91
Ben Biddington <ben.biddington@gmail.com>
2011-03-16 13:56:44 +1300

    Removed more generated garbage, unused and empty files

--- features/step_definitions/global_steps.rb
+++ features/step_definitions/global_steps.rb
@@ -1,18 +1,11 @@
-# Given /^the following (.+) records?$/ do |factory, table|
-#   # table is a Cucumber::Ast::Table
-#   table.hashes.each do |hash|
-#     Factory(factory,hash)
-#   end
-# end
-# 
 Given /^I am logged in as ([^\"]*)$/ do |username|
   Then "Login in as #{username} with password #{username}"
+  assert_not_contain "Invalid user or password"
 end
-
 
 Given /^Login in as ([^\"]*) with password ([^\"]*)$/ do |username, password|
   visit url_for(:controller => 'account', :action => 'login')
-  fill_in "Login", :with => username
-  fill_in "Password", :with => password
-  click_button "Login"
+  fill_in "username", :with => username
+  fill_in "password", :with => password
+  click_button "login"
 end
