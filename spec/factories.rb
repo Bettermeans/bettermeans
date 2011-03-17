2c94775e6c6ea7172379005fe6461a313a35dae6
Ben Biddington <ben.biddington@gmail.com>
2011-03-18 11:26:38 +1300

    Re-added setting trackers because some tests rely on it

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,7 @@ Factory.define :project do |f|
   f.sequence(:name) { |n| "Name of project #{n}" }
   f.sequence(:identifier) { |n| "workstreamid#{n}" }
   f.status 1
-  f.trackers nil
+  f.trackers Tracker.all
   f.is_public true
   f.enabled_module_names Redmine::AccessControl.available_project_modules
 end
