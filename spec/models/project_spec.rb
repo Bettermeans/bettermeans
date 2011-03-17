4f3aee8e3465fbc4404a8710f2c05884c3595e4d
Ben Biddington <ben.biddington@gmail.com>
2011-03-18 11:37:52 +1300

    Project#visible_by returns project status and publicity filter if user is not supplied

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -9,4 +9,9 @@ describe Project,"#visible_by" do
     result = Project.visible_by fake_user.new
     result.should eql "projects.status=1"
   end
+  
+  it "returns project status and publicity filter if user is not supplied" do 
+    result = Project.visible_by
+    result.should eql "projects.status=1 AND projects.is_public = 't'"
+  end
 end
