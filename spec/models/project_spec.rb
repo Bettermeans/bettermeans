ddea925dc2a6b5fef2e3383c990aac5382584085
Ben Biddington <ben.biddington@gmail.com>
2011-03-27 21:18:12 +1300

    Added Project#{latest,most_active}_public

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -50,6 +50,50 @@ describe Project,"#visible_by" do
   end
 end
 
+describe Project,"#latest_public" do 
+  it "only queries for root projects, i.e., projects that are not children of other projects" do
+    fake_named_scope = mock("Fake named scope")
+    fake_named_scope.stub(:find).with(any_args)
+      
+    Project.should_receive(:all_roots).once.and_return fake_named_scope
+    Project.should_not_receive(:all_children)
+      
+    Project.latest_public fake_admin
+  end
+  
+  it "requires user argument" do
+    lambda{Project.latest_public nil}.should raise_error "The \"user\" argument is required."
+  end
+  
+  def fake_admin
+    result = mock("A fake admin")
+    result.stub(:admin?).and_return true
+    result
+  end
+end
+
+describe Project,"#most_active_public" do 
+  it "only queries for root projects, i.e., projects that are not children of other projects" do
+    fake_named_scope = mock("Fake named scope")
+    fake_named_scope.stub(:find).with(any_args)
+      
+    Project.should_receive(:all_roots).once.and_return fake_named_scope
+    Project.should_not_receive(:all_children)
+      
+    Project.most_active_public fake_admin
+  end
+  
+  it "requires user argument" do
+    lambda{Project.most_active_public nil}.should raise_error "The \"user\" argument is required."
+  end
+  
+  def fake_admin
+    result = mock("A fake admin")
+    result.stub(:admin?).and_return true
+    result
+  end
+end
+
 describe User.anonymous," user type" do 
     it "has zero memberships" do 
       User.anonymous.memberships.any?.should be_false
