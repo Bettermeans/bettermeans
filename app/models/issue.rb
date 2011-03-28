c2508fac1206c28c5e0013471934ef3c74446de7
Shereef Bishay <shereef@gmail.com>
2011-03-28 13:49:21 -0700

    Adds tagsinput

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -27,6 +27,8 @@ class Issue < ActiveRecord::Base
                      # sort by id so that limited eager loading doesn't break with postgresql
                      :order_column => "#{table_name}.id"
 
+  acts_as_taggable
+
   acts_as_event :title => Proc.new {|o| "#{o.tracker.name} ##{o.id} (#{o.status}): #{o.subject}"},
                 :url => Proc.new {|o| {:controller => 'issues', :action => 'show', :id => o.id}},
                 :type => Proc.new {|o| 'issue' + (o.closed? ? ' closed' : '') }
