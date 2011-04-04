ce4ffcd4246526b03e99e55c8c302fec2ea92040
Shereef Bishay <shereef@gmail.com>
2011-04-04 12:05:11 -0700

    Shuts down new signups

--- config/routes.rb
+++ config/routes.rb
@@ -320,7 +320,7 @@ ActionController::Routing::Routes.draw do |map|
   #semi-statig pages
   map.root :controller => 'home'
   map.home '', :controller => 'home', :action => 'index'
-  map.static '/front/:page', :controller => 'home', :action => 'show', :page => /index.html|about.html|howitworks.html|contact.html|hq.html|pricing.html|signup.html|apps.html|products.html|services.html|single.html|tour.html|webdesign.html|index.htm|elements.html|privacy.html|library.html|testimonials.html|irb.html|open_enterprise_governance_model.html|user_agreement.html|why.html|how.html|what.html/                          
+  map.static '/front/:page', :controller => 'home', :action => 'show', :page => /index.html|about.html|howitworks.html|contact.html|hq.html|pricing.html|signup.html|apps.html|products.html|services.html|single.html|tour.html|webdesign.html|index.htm|elements.html|privacy.html|library.html|testimonials.html|irb.html|open_enterprise_governance_model.html|user_agreement.html|why.html|how.html|what.html|inviteonly.html/                          
   
     
   # Install the default route as the lowest priority.
