07bbe2c5cbb050caa5f2447a5581b87770629445
Ben Biddington <ben.biddington@gmail.com>
2011-04-13 11:53:15 +1200

    Described how ProjectsController#index_active works with Project

---
+++ spec/controllers/projects/index_active_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'                                                                                                                                        
+
+describe ProjectsController,"#index_active" do
+  before :each do
+    controller.stub(:respond_to)
+    controller.params[:offset] = 5
+  end
+  
+  it "finds the 10 most active workstreams only" do             
+    Project.should_receive(:most_active).with(10, anything)
+    Project.should_not_receive(:latest_public)
+        
+    controller.index_active
+  end
+  
+  it "returns most active workstreams (as active_enterprises)" do      
+    expected_most_active = "xxx"
+    
+    Project.should_receive(:most_active).with(10, anything).and_return expected_most_active 
+    
+    controller.index_active
+    
+    controller.instance_variable_get(:@active_enterprises).should eql expected_most_active
+  end
+  
+  it "uses the offset parameter from query string" do 
+    expected_offset = 1337
+        
+    controller.params[:offset] = expected_offset
+        
+    Project.should_receive(:most_active).with(anything, expected_offset)
+        
+    controller.index_active  
+  end
+end
