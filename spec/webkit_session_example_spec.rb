d6f3cae2c8953e644445aa61bafd24c6e9094fc0
Ben Biddington <ben.biddington@gmail.com>
2011-04-23 10:25:55 +1200

    WebkitSession#get resets its driver when get is called

--- spec/webkit_session_example_spec.rb
+++ spec/webkit_session_example_spec.rb
@@ -10,7 +10,7 @@ describe WebkitSession,"#click" do
   it "fails if the element cannot be found" do
     session = WebkitSession.new @driver
     
-    @driver.stub(:find).once.and_return []
+    driver.stub(:find).once.and_return []
     
     lambda{session.click "xxx"}.should raise_error "Unable to locate element <xxx>"
   end
@@ -18,12 +18,16 @@ describe WebkitSession,"#click" do
   it "fails if there are multiple elements found" do 
     session = WebkitSession.new @driver
     
-    @driver.stub(:find).once.and_return [Object.new,Object.new]
+    driver.stub(:find).once.and_return [Object.new,Object.new]
     
     lambda{session.click "xxx"}.should raise_error "Too many elements found. Expected <1>. Got <2>."  
   end
   
   it "you can click anything"
+  
+  private
+  
+  attr_reader :driver
 end
 
 describe WebkitSession,"#find" do
@@ -34,9 +38,9 @@ describe WebkitSession,"#find" do
   it "tries to find elements by id, name and value" do
     element_identifier = "xxx"
     
-    @driver.should_receive(:find).with("//*[@id='#{element_identifier}']").and_return [] 
-    @driver.should_receive(:find).with("//*[@name='#{element_identifier}']").and_return [] 
-    @driver.should_receive(:find).with("//*[@value='#{element_identifier}']").and_return [] 
+    driver.should_receive(:find).with("//*[@id='#{element_identifier}']").and_return [] 
+    driver.should_receive(:find).with("//*[@name='#{element_identifier}']").and_return [] 
+    driver.should_receive(:find).with("//*[@value='#{element_identifier}']").and_return [] 
     
     session.find element_identifier
   end
@@ -44,19 +48,37 @@ describe WebkitSession,"#find" do
   it "returns nil if element cannot be found by id, name or value" do 
     element_identifier = "xxx"
     
-    @driver.should_receive(:find).with("//*[@id='#{element_identifier}']").and_return [] 
-    @driver.should_receive(:find).with("//*[@name='#{element_identifier}']").and_return [] 
-    @driver.should_receive(:find).with("//*[@value='#{element_identifier}']").and_return [] 
+    driver.should_receive(:find).with("//*[@id='#{element_identifier}']").and_return [] 
+    driver.should_receive(:find).with("//*[@name='#{element_identifier}']").and_return [] 
+    driver.should_receive(:find).with("//*[@value='#{element_identifier}']").and_return [] 
     
     result = session.find element_identifier
     result.should be_nil
   end
   
+  private 
+  attr_reader :driver
+  def session; @session ||= WebkitSession.new @driver; end
+end
+
+describe WebkitSession,"#get" do
+  before :each do 
+    @driver = stub("A fake driver")  
+  end
+  
+  it "resets its driver when get is called" do
+    driver.should_receive(:reset!).once
+    driver.stub(:visit)
+    session.get "http://xxx.com"  
+  end
+  
+  private
+  
+  attr_reader :driver
   def session; @session ||= WebkitSession.new @driver; end
 end
 
 describe WebkitSession do
-  it "resets its driver when get is called"
   it "clears all scope when get is called"
   it "supplies a dummy 'response' argument (not nil) when invoking Webrat::Scope.from_page to stop response caching error"
 end
