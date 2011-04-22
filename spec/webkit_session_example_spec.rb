a515f602cba68c3b05139e27fc047240d8d57809
Ben Biddington <ben.biddington@gmail.com>
2011-04-22 17:19:00 +1200

    WebkitSession#click fails if the element cannot be found

--- spec/webkit_session_example_spec.rb
+++ spec/webkit_session_example_spec.rb
@@ -2,11 +2,23 @@ require "spec_helper"
 
 require "features/support/webkit_session.rb"
 
+describe WebkitSession,"#click" do
+  it "fails if the element cannot be found" do
+    stub_driver = stub("A fake driver")
+    session = WebkitSession.new stub_driver
+    
+    stub_driver.stub(:find).once.and_return []
+    
+    lambda{session.click "xxx"}.should raise_error "Unable to locate element <xxx>"
+  end
+  
+  it "fails if there are multiple elements found"
+  it "you can click anything"
+end
+
 describe WebkitSession do
   it "resets its driver when get is called"
   it "clears all scope when get is called"
+  it "Tries to find elements by id, name and value"
   it "supplies a dummy 'response' argument (not nil) when invoking Webrat::Scope.from_page to stop response caching error"
-  
-  it "click_button fails unless the specified thing can be found"
-  it "you can click anything"
 end
