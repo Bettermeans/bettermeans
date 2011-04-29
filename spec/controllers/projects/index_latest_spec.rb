9b6f1d564f284218e1c15ca5975726b850cbafee
Ben Biddington <ben.biddington@gmail.com>
2011-04-29 18:30:14 +1200

    Modified to follow the same form as other controller tests (the get :action style)

--- spec/controllers/projects/index_latest_spec.rb
+++ spec/controllers/projects/index_latest_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe ProjectsController,"#index_latest" do
   before :each do
+    login
     controller.stub(:respond_to) # TODO: pretty lame to have to do this. Hidden collaborators.
     controller.params[:offset] = 5
   end
@@ -10,7 +11,7 @@ describe ProjectsController,"#index_latest" do
     Project.should_receive(:latest_public).with(10, anything)
     Project.should_not_receive(:most_active_public)
         
-    controller.index_latest
+    get :index_latest
   end
   
   it "returns latest public workstreams (as latest_enterprises)" do      
@@ -18,18 +19,16 @@ describe ProjectsController,"#index_latest" do
     
     Project.should_receive(:latest_public).with(10, anything).and_return expected_latest_public 
     
-    controller.index_latest
+    get :index_latest
     
     controller.instance_variable_get(:@latest_enterprises).should eql expected_latest_public
   end
   
   it "uses the offset parameter from query string" do 
     expected_offset = 1337
-        
-    controller.params[:offset] = expected_offset
-        
+            
     Project.should_receive(:latest_public).with(anything, expected_offset)
         
-    controller.index_latest  
+    get :index_latest, :offset => expected_offset  
   end
 end
