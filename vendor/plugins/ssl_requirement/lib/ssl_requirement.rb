d1ffe1b639ec7be63d0880b3a46a119e2b803c20
elf Pavlik <perpetual-tripper@wwelves.org>
2011-11-13 15:18:43 +0100

    reintroduced customization to support :all (org. added in 2c6332853b)

--- vendor/plugins/ssl_requirement/lib/ssl_requirement.rb
+++ vendor/plugins/ssl_requirement/lib/ssl_requirement.rb
@@ -44,7 +44,7 @@ module SslRequirement
   protected
     # Returns true if the current action is supposed to run as SSL
     def ssl_required?
-      (self.class.read_inheritable_attribute(:ssl_required_actions) || []).include?(action_name.to_sym)
+      (self.class.read_inheritable_attribute(:ssl_required_actions) || []).include?(action_name.to_sym) || (self.class.read_inheritable_attribute(:ssl_required_actions) || []).include?(:all)
     end
     
     def ssl_allowed?
