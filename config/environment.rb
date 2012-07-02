8019df4f1b498a13aa64ec6eaead2fe61114e4c7
Robert Fletcher <lobatifricha@gmail.com>
2012-07-01 17:54:50 -0700

    updating deprecated behaviors

--- config/environment.rb
+++ config/environment.rb
@@ -7,10 +7,6 @@
 #For ruby debug
 SCRIPT_LINES__ = {} if ENV['RAILS_ENV'] == 'development'
 
-
-# Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
-
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
@@ -29,7 +25,7 @@ Rails::Initializer.run do |config|
   # config.frameworks -= [ :action_web_service, :action_mailer ]
 
   # Add additional load paths for sweepers
-  config.load_paths += %W( #{RAILS_ROOT}/app/sweepers )
+  config.autoload_paths += %W( #{RAILS_ROOT}/app/sweepers )
 
   # Force all environments to use the same logger level 
   # (by default production uses :info, the others :debug)
