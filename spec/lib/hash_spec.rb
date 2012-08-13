ab3e2d75de86f8091da5325c737dff13689330aa
Robert Fletcher <lobatifricha@gmail.com>
2012-08-12 22:41:24 -0700

    account for inconsistent hash order in 1.8.7

--- spec/lib/hash_spec.rb
+++ spec/lib/hash_spec.rb
@@ -1,14 +1,27 @@
 require 'spec_helper'
 
 describe Hash do
-  describe "to_array_conditions" do
+  describe "#to_array_conditions" do
     it "should load individual values" do
-      {:v1 => 1, :v2 => '2'}.to_array_conditions.should == ["v1 = ? AND v2 = ?",'1','2']
+      hash = {:v1 => 1, :v2 => '2'}
+
+      possible_results = [
+        ["v1 = ? AND v2 = ?",'1','2'],
+        ["v2 = ? AND v1 = ?",'2','1'],
+      ]
+
+      possible_results.should include(hash.to_array_conditions)
     end
 
     it "should use in for array values" do
-      {:v1 => 1, :v2 => [2,'two']}.to_array_conditions.should ==
-        ["v1 = ? AND v2 in (?)", '1', [2,'two']]
+      hash = {:v1 => 1, :v2 => [2,'two']}
+
+      possible_results = [
+        ["v1 = ? AND v2 in (?)", '1', [2,'two']],
+        ["v2 in (?) AND v1 = ?", [2,'two'], '1'],
+      ]
+
+      possible_results.should include(hash.to_array_conditions)
     end
   end
 end
