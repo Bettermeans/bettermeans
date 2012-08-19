23b5b239f63a8d6d0ed8e35b810a75241c92346d
Robert Fletcher <lobatifricha@gmail.com>
2012-08-19 16:47:07 -0700

    add welcome_routing_spec.rb

---
+++ spec/routing/welcome_routing_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe WelcomeController do
+
+  describe "routing" do
+
+    it "routes to #index" do
+      route_for(:controller => "welcome", :action => "index").should == "/welcome"
+    end
+
+    it "routes to #new" do
+      route_for(:controller => "welcome", :action => "robots").should == "/welcome/robots"
+    end
+
+  end
+
+end
