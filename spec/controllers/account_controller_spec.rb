99aba9c86f043067979ceb5be90d618491b282d5
Robert Fletcher <lobatifricha@gmail.com>
2012-08-19 19:31:31 -0700

    working on account_controller_spec.rb

--- spec/controllers/account_controller_spec.rb
+++ spec/controllers/account_controller_spec.rb
@@ -1,11 +1,40 @@
 require 'spec_helper'
 
-describe AccountController,"#login" do
+describe AccountController do
+  before :each do
+    @request.env['HTTPS'] = 'on'
+  end
 
-  it "should redirect to https on http request" do
-    @request.env['HTTPS'] = nil
-    get :login
-    response.should redirect_to "https://" + @request.host + @request.request_uri
+  describe "#login" do
+
+    context "on an http request" do
+      it "redirects to https" do
+        @request.env['HTTPS'] = nil
+        get(:login)
+        url = "https://#{@request.host}#{@request.request_uri}"
+        response.should redirect_to url
+      end
+    end
+
+    context "given an invitation token in the session" do
+      context "with invitation token passed as a param" do
+        it "assigns the params version to @invitation_token" do
+          session[:invitation_token] = 'stuff'
+          get(:login, :invitation_token => 'blah')
+          assigns(:invitation_token).should == 'blah'
+        end
+      end
+
+      context "without invitiation token passed as param" do
+        it "assigns the session version to @invitation_token" do
+          session[:invitation_token] = 'stuff'
+          get(:login)
+          assigns(:invitation_token).should == 'stuff'
+        end
+
+      end
+    end
+
   end
 
 end
