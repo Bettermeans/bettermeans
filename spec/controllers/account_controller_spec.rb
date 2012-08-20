6a18723d3e9ea51d0fff1aa8f8e7c412cbda7424
Robert Fletcher <lobatifricha@gmail.com>
2012-08-19 23:28:54 -0700

    flesh out #login specs

--- spec/controllers/account_controller_spec.rb
+++ spec/controllers/account_controller_spec.rb
@@ -31,7 +31,43 @@ describe AccountController do
           get(:login)
           assigns(:invitation_token).should == 'stuff'
         end
+      end
+    end
 
+    context "when request is a GET" do
+      it "logs out the user" do
+        user = Factory.create(:user)
+        User.current = user
+        get(:login)
+        User.current.should == User.anonymous
+      end
+
+      it "sets the session invitation_token" do
+        get(:login, :invitation_token => 'blah')
+        session[:invitation_token].should == 'blah'
+      end
+
+      it "renders the static layout" do
+        get(:login)
+        response.layout.should == 'layouts/static'
+      end
+    end
+
+    context "when request is not a GET" do
+      context "when openid" do
+        it "authenticates via openid" do
+          Setting.should_receive(:openid?).and_return(true)
+          controller.should_receive(:using_open_id?).and_return(true)
+          controller.should_receive(:open_id_authenticate).with('blah')
+          post(:login, :openid_url => 'blah')
+        end
+      end
+
+      context "when not openid" do
+        it "authenticates via password" do
+          controller.should_receive(:password_authentication).with('blah')
+          post(:login, :invitation_token => 'blah')
+        end
       end
     end
 
