1302edb9c9dca6a10a4e97ac9530f95be4771b50
Robert Fletcher <lobatifricha@gmail.com>
2012-08-19 17:13:01 -0700

    fix action name

--- spec/routing/welcome_routing_spec.rb
+++ spec/routing/welcome_routing_spec.rb
@@ -8,7 +8,7 @@ describe WelcomeController do
       route_for(:controller => "welcome", :action => "index").should == "/welcome"
     end
 
-    it "routes to #new" do
+    it "routes to #robots" do
       route_for(:controller => "welcome", :action => "robots").should == "/welcome/robots"
     end
 
