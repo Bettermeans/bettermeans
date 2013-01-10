deb003dd5672cfa903514eb35d5044f84eb9af90
Tom Uebel <tauebel@gmail.com>
2013-01-10 15:41:07 -0800

    writing acceptance tests

--- spec/acceptance/user_login_spec.rb
+++ spec/acceptance/user_login_spec.rb
@@ -12,5 +12,20 @@ feature "Feature name", %q{
     click_link 'sign up'
     page.should have_content('Plans and Pricing')
     page.should have_content('Pricing FAQ’s')
+    within '.free-box' do
+      click_link 'Sign up'
+    end
+    page.should have_content('or create an account')
+    fill_in 'Username', :with => 'example'
+    fill_in 'Password', :with => '123456'
+    fill_in 'Retype Password', :with => '123456'
+    fill_in 'Firstname', :with => 'John'
+    fill_in 'Lastname', :with => 'Doe'
+    fill_in 'Email', :with => 'example@example.org'
+    click_button 'Sign up'
+    page.should have_content('Account was successfully created')
+    user = User.find_by_mail('example@example.org')
+    user.status = User::STATUS_ACTIVE
+    user.save
   end
 end
