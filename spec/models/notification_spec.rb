6d3169c12b8c5cb60b8544c1d00392a7d6814329
dklounge <daviddarden11@gmail.com>
2013-08-09 10:24:16 -0700

    added spec for mark_as_responded method

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -2,4 +2,30 @@ require "spec_helper"
 
 describe Notification do
 
+  describe "#mark_as_responded" do
+    let(:notification) { Notification.new }
+
+    it "sets the state to responded" do
+      notification.state = Notification::STATE_ACTIVE
+      notification.mark_as_responded
+      notification.state.should == Notification::STATE_RESPONDED
+    end
+
+    it "sets the params to nil" do
+      notification.params = 1
+      notification.mark_as_responded
+      notification.params.should == nil
+    end
+
+    it "saves the record" do
+      notification.should_receive(:save)
+      notification.mark_as_responded
+    end
+  end
+
+  describe "#mention?" do
+    it "" do
+    end
+  end
+
 end
