3a56638af4476041be860819e7b19da85452c56e
Joshua Wilborn <Joshua.J.Wilborn@gmail.com>
2013-09-23 04:47:17 -0700

    add board spec

---
+++ spec/models/board_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+
+describe Board do
+  let(:board) do
+    Board.create!(
+      {:name => 'test name',
+       :description => 'test desc',
+       :project_id => 1
+      }
+    )
+  end
+
+  let(:user) { User.new }
+
+  describe "#visible?" do
+    it "returns true if the user is allowed to view messages on the project" do
+      User.stub(:current).and_return(user)
+      user.stub(:allowed_to?).and_return(true)
+      board.should be_visible
+    end
+
+    it "returns false if the user is not allowed to view messages on the project" do
+      User.stub(:current).and_return(user)
+      user.stub(:allowed_to?).and_return(false)
+      board.should_not be_visible
+    end
+  end
+
+  describe "#to_s" do
+    it "should be the board name" do
+      board.to_s.should == board.name
+    end
+  end
+
+  describe "#reset_counters!" do
+    it "calls .reset_counters!" do
+      Board.should_receive(:reset_counters!).with(board.id)
+      board.reset_counters!
+    end
+  end
+
+  describe ".reset_counters" do
+    it "updates topics_count and message_count" do
+      board.reset_counters!
+      board.reload.topics_count.should == 0
+      Message.create!({:board_id => board.id, :subject => 'text', :content => 'text'})
+      Message.create!({:board_id => board.id, :subject => 'text', :content => 'text', :parent_id => 1})
+      board.reset_counters!
+      board.reload.topics_count.should == 1
+      board.messages_count.should == 2
+    end
+
+    it "updates last_message_id" do
+      message = Message.create!({:board_id => board.id,
+                                 :subject => 'text',
+                                 :content => 'text',
+                                 :parent_id => 1})
+      board.reset_counters!
+      board.reload.last_message_id.should == message.id
+    end
+  end
+end
