01e45a41816904a36d969d9f264c6e13c06268fa
dklounge <daviddarden11@gmail.com>
2013-09-24 23:18:51 -0700

    fixed wiki#find_or_new_page model spec

---
+++ spec/models/wiki_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Wiki do
+
+  describe "find_or_new_page(title)" do
+    let(:wiki) { Wiki.create!(:project_id => 5, :start_page => 'something') }
+
+    describe '#find_or_new_page' do
+      it 'creates a new wiki page' do
+        wiki_page = wiki.find_or_new_page('title')
+        wiki_page.title.should == 'title'.titleize
+      end
+    end
+  end
+
+end
