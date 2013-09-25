b9b3725af10af91d8a5c01f761305e11a99ab145
dklounge <daviddarden11@gmail.com>
2013-09-25 00:27:50 -0700

    added tracker spec

---
+++ spec/models/tracker_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Tracker do
+  it { should have_many(:issues) }
+  it { should have_many(:workflows) }
+  it { should have_and_belong_to_many(:projects) }
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name) }
+
+  describe '#to_s' do
+    let(:tracker) { Tracker.new(:name => "great_name") }
+    it 'returns stringified object' do
+      tracker.name.should == "great_name"
+    end
+  end
+end
--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Wiki do
+  it { should have_many(:pages).dependent(:destroy) }
+  it { should have_many(:redirects).dependent(:delete_all) }
 
   describe "find_or_new_page(title)" do
     let(:wiki) { Wiki.create!(:project_id => 5, :start_page => 'something') }
