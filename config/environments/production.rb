c92aadef9c11523b2e1b317595b47683efc356fd
Robert Fletcher <lobatifricha@gmail.com>
2013-10-20 18:20:13 -0700

    set up email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,12 +19,13 @@ config.action_controller.perform_caching             = true
 config.action_mailer.raise_delivery_errors = false
 
 ActionMailer::Base.smtp_settings = {
-  :address        => "smtp.sendgrid.net",
-  :port           => "25",
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => ENV['SENDGRID_DOMAIN'],
+  :address              => "smtp.sendgrid.net",
+  :port                 => 587,
+  :authentication       => :plain,
+  :user_name            => ENV['SENDGRID_USERNAME'],
+  :password             => ENV['SENDGRID_PASSWORD'],
+  :domain               => ENV['SENDGRID_DOMAIN'],
+  :enable_starttls_auto => true,
 }
 
 # No email in production log
