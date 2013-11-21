2724e45b8fa7e10e5ef16a018dd4e54ac28216c3
David Kim <daviddarden11@gmail.com>
2013-11-21 14:54:40 -0800

    updated query spec

--- spec/models/query_column_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe QueryColumn do
-
-end
--- spec/models/query_spec.rb
+++ spec/models/query_spec.rb
@@ -14,7 +14,31 @@ describe Query do
     it { should ensure_length_of(:name).is_at_most(255) }
   end
 
-  describe 'after_initialize' do
+  # should project be stubbed? what should be returned?
+  describe '#after_initialize' do
+    context 'if project is nil' do
+      it 'stores nil project and return @is_for_all' do
+        query.after_initialize.should be_true
+      end
+    end
+  end
+
+  # not sure how this one works
+  # 1. how one would know the output without console
+  # 2. what the point of setting up method like this would be
+  describe '#has_filter?' do
+    it 'returns filters and filters[field]' do
+      field = 'status_id'
+      query.has_filter?(field).should ==
+        {:operator=>"o", :values=>[""]}
+    end
+  end
+
+  describe '#grouped?' do
+    it 'returns true if query is a grouped query' do
+      query.group_by = true
+      query.grouped?.should be_true
+    end
   end
 
 end
