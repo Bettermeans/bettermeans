94ac84cff88969b0719fa13604b34ed12c8554c0
Robert Fletcher <lobatifricha@gmail.com>
2014-03-29 16:13:41 -0700

    dusting off steak/capybara

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -1,10 +1,8 @@
-require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
-require "steak"
+require 'spec_helper'
 require 'capybara/rails'
 
 Spec::Runner.configure do |config|
-  config.include Capybara
+  config.include Capybara::DSL
 end
 
-# Put your acceptance spec helpers inside /spec/acceptance/support
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
--- spec/acceptance/user_login_spec.rb
+++ spec/acceptance/user_login_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
+require 'acceptance/acceptance_helper'
 
 feature "Feature name", %q{
   In order to ...
