385031580ef992fade0112fa6e726e964940a68a
Robert Fletcher <lobatifricha@gmail.com>
2014-10-05 12:02:18 -0700

    fix AccountController#activate spec to integrate_views

--- spec/controllers/account_controller/activate_spec.rb
+++ spec/controllers/account_controller/activate_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe AccountController, '#activate' do
 
-  context "if self_registration is not set" do
-    it "redirects to home_url" do
+  integrate_views
+
+  context 'if self_registration is not set' do
+    it 'redirects to home_url' do
       user = Factory.create(:user, :status => User::STATUS_REGISTERED)
       token = Token.create(:user => user, :action => 'register')
       Setting.stub(:self_registration?).and_return(false)
@@ -12,8 +14,8 @@ describe AccountController, '#activate' do
     end
   end
 
-  context "if params[:token] is not present" do
-    it "redirects to home_url" do
+  context 'if params[:token] is not present' do
+    it 'redirects to home_url' do
       user = Factory.create(:user, :status => User::STATUS_REGISTERED)
       token = Token.create(:user => user, :action => 'register')
       Setting.stub(:self_registration?).and_return(true)
@@ -22,16 +24,16 @@ describe AccountController, '#activate' do
     end
   end
 
-  context "if the token is not found" do
-    it "redirects to home_url" do
+  context 'if the token is not found' do
+    it 'redirects to home_url' do
       Setting.stub(:self_registration?).and_return(true)
       get(:activate, :token => 'blah')
       response.should redirect_to(home_url)
     end
   end
 
-  context "if the token is found but expired" do
-    it "redirects to home_url" do
+  context 'if the token is found but expired' do
+    it 'redirects to home_url' do
       Setting.stub(:self_registration?).and_return(true)
       user = Factory.create(:user, :status => User::STATUS_REGISTERED)
       token = Token.create(:user => user, :action => 'register')
@@ -44,8 +46,8 @@ describe AccountController, '#activate' do
     end
   end
 
-  context "if the user is not registered" do
-    it "redirects to home_url" do
+  context 'if the user is not registered' do
+    it 'redirects to home_url' do
       Setting.stub(:self_registration?).and_return(true)
       user = Factory.create(:user, :status => User::STATUS_ACTIVE)
       token = Token.create(:user => user, :action => 'register')
@@ -54,7 +56,7 @@ describe AccountController, '#activate' do
     end
   end
 
-  context "if the user is valid" do
+  context 'if the user is valid' do
     let(:user) { Factory.create(:user, :status => User::STATUS_REGISTERED) }
     let(:token) { Token.create(:user => user, :action => 'register') }
 
@@ -62,23 +64,34 @@ describe AccountController, '#activate' do
       Setting.stub(:self_registration?).and_return(true)
     end
 
-    it "changes the user's status to active" do
+    it 'changes the status of the user to active' do
       get(:activate, :token => token.value)
       user.reload.status.should == User::STATUS_ACTIVE
     end
 
-    it "destroys the token" do
+    it 'destroys the token' do
       get(:activate, :token => token.value)
       Token.find_by_id(token.id).should_not be
     end
 
-    it "authenticates the user" do
-      controller.should_receive(:successful_authentication).with(user)
+    it 'logs in the user' do
       get(:activate, :token => token.value)
+      controller.current_user.should == user
+    end
+
+    it 'redirects to welcome/index' do
+      get(:activate, :token => token.value)
+      response.should redirect_to(:controller => :welcome, :action => :index)
+    end
+
+    it 'redirects back given a back_url' do
+      back_url = 'test.host/what/the/heck'
+      get(:activate, :token => token.value, :back_url => back_url)
+      response.should redirect_to(back_url)
     end
   end
 
-  context "if the user is invalid" do
+  context 'if the user is invalid' do
     let(:user) { Factory.create(:user, :status => User::STATUS_REGISTERED) }
     let(:token) { Token.create(:user => user, :action => 'register') }
 
@@ -90,11 +103,11 @@ describe AccountController, '#activate' do
       get(:activate, :token => token.value)
     end
 
-    it "renders the login page" do
+    it 'renders the login page' do
       response.should render_template('login')
     end
 
-    it "renders the static layout" do
+    it 'renders the static layout' do
       response.layout.should == 'layouts/static'
     end
   end
