9ca97233787cb23993f9632aaebc0aacc0b9b7b5
Robert Fletcher <lobatifricha@gmail.com>
2015-01-24 17:05:39 -0800

    fix wiki page specs

--- spec/controllers/wiki_controller/index_spec.rb
+++ spec/controllers/wiki_controller/index_spec.rb
@@ -4,8 +4,11 @@ describe WikiController, '#index' do
 
   let(:user) { Factory.create(:user) }
   let(:project) { Factory.create(:project) }
-  let(:wiki) { Factory.create(:wiki, :project => project) }
-  let(:wiki_page) { Factory.create(:wiki_page, :wiki => wiki, :title => 'wat') }
+  let(:wiki) { project.wiki }
+  let(:wiki_page) { wiki.pages.create!(:title => 'wat') }
+  let!(:wiki_content) do
+    WikiContent.create!(:text => 'some text', :page => wiki_page)
+  end
   let(:valid_params) { { :page => wiki_page.title, :id => project.id } }
 
   before(:each) do
@@ -23,7 +26,7 @@ describe WikiController, '#index' do
 
     context 'when the user is not allowed to edit wiki pages for the project' do
       it 'renders a 404' do
-        get(:index, valid_params)
+        get(:index, valid_params.merge(:page => 'foo'))
         response.status.should == '404 Not Found'
       end
     end
@@ -32,8 +35,11 @@ describe WikiController, '#index' do
   context 'when given a params[:version]' do
     context 'when user is not allowed to view wiki edits' do
       it 'redirects to the current version' do
+        non_member_role = Role.find_by_name('Non member')
+        non_member_role.permissions -= [:view_wiki_edits]
+        non_member_role.save!
         get(:index, valid_params.merge(:version => 'pie'))
-        response.should redirect_to('butts')
+        response.should redirect_to(:action => :index)
       end
     end
   end
--- spec/factories/wiki_pages.rb
+++ spec/factories/wiki_pages.rb
@@ -1,3 +1,4 @@
 Factory.define :wiki_page do |f|
   f.association :wiki, :factory => :wiki
+  f.title 'wat'
 end
